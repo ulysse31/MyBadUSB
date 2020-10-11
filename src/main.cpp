de0ac6938ff268becac466a6e67ca45cf4309364
ulysse31 <ulysse31@gmail.com>
2020-10-11 16:20:48 +0200

    Updated initial timing

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ void setup() {
   // Keyboard library start
   Keyboard.begin();
   // Some delay is needed just to be sure that usb keyboard gets enabled on Host Operating system (may vary between target)
-  delay(1200);
+  delay(1800);
 }
 
 void loop()
