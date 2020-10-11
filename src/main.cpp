90bd89c5110c0240c0f9f7869f74d53cc4ed7cf9
ulysse31 <ulysse31@gmail.com>
2020-10-11 16:05:48 +0200

    Removed Keyboard.end() : no need (less alerts) ^^'

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,6 @@ void loop()
   Keyboard.print("exit");
   Keyboard.press(KEY_RETURN);
   Keyboard.release(KEY_RETURN);
-  Keyboard.end();
   // We are in a awareness campaign, this is enough so we stop here and wait forever ^^ 
   delay(4000000000);
 }
