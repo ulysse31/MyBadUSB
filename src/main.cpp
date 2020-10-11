5373db3f1ce0e24de5863b3a1cc27b04a7c7c419
ulysse31 <ulysse31@gmail.com>
2020-10-11 16:14:21 +0200

    re-organized timings for a better "human like" typing #UltraFastHumanType Added/Corrected some comments

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 void setup() {
   // Keyboard library start
   Keyboard.begin();
-  // Some delay is needed just to be sure that usb keyboard gets enabled on Operating system
+  // Some delay is needed just to be sure that usb keyboard gets enabled on Host Operating system (may vary between target)
   delay(1200);
 }
 
@@ -22,45 +22,47 @@ void loop()
   // type & launch powershell.exe
   Keyboard.print("powershell.exe");
   Keyboard.press(KEY_RETURN);
+  delay(10);
   Keyboard.release(KEY_RETURN);
+  // waiting longer for powershell instanciation (depends on target speed)
   delay(200);
   //
   // Powershell payload HERE: sample payload, works great on Microsoft Enabled O365 / Exchange Companies
   Keyboard.print("$Outlook = New-Object -ComObject Outlook.Application");
   delay(10);
   Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);
   delay(10);
+  Keyboard.release(KEY_RETURN);
   Keyboard.print("$Mail = $Outlook.CreateItem(0)");
   delay(10);
   Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);
   delay(10);
+  Keyboard.release(KEY_RETURN);
   Keyboard.print("$Mail.To = ");
   Keyboard.print(ALERT_MAIL);
   delay(10);
   Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);
   delay(10);
+  Keyboard.release(KEY_RETURN);
   Keyboard.print("$Mail.Subject = \"I'm a naughty person\"");
   delay(10);
   Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);
   delay(10);
+  Keyboard.release(KEY_RETURN);
   Keyboard.print("$Mail.Body =\"Sorry, I'm naughty I plugged in a unknown usb key\"");
   delay(10);
   Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);
   delay(10);
+  Keyboard.release(KEY_RETURN);
   Keyboard.print("$Mail.Send()");
-  delay(10);
   Keyboard.press(KEY_RETURN);
-  Keyboard.release(KEY_RETURN);
   delay(10);
+  Keyboard.release(KEY_RETURN);
   // End Powershell payload
   //
   Keyboard.print("exit");
   Keyboard.press(KEY_RETURN);
+  delay(10);
   Keyboard.release(KEY_RETURN);
   // We are in a awareness campaign, this is enough so we stop here and wait forever ^^ 
   delay(4000000000);
